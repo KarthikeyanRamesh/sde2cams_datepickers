de636a9abf4ce763a6984637d9a8c3e09c62312a
KarthikeyanRamesh <karthisujatha10@gmail.com>
2025-06-18 13:54:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PublishData from './components/PublishData';
+
+function App() {
+  return (
+    <div>
+      <PublishData />
+    </div>
+  );
+}
+
+export default App;
